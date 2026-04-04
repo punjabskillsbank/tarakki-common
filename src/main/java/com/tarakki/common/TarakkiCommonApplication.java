4591f7d08aa9fa9d6764e32aaa4823930b4a0597
Ivkaran Singh <ivkaransingh@macbook-pro.mynetworksettings.com>
2026-04-04 16:55:20 -0400

    first commit

---
+++ src/main/java/com/tarakki/common/TarakkiCommonApplication.java
@@ -0,0 +1,13 @@
+package com.tarakki.common;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TarakkiCommonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TarakkiCommonApplication.class, args);
+	}
+
+}
