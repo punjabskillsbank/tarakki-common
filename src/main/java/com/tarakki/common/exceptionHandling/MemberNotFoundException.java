98836971b6ccadec5980aa9120b793a1200f3cf7
ishmeet-codes <ishmeetsinghxa@gmail.com>
2026-06-12 21:40:12 +0530

    TK-37: Added the exception for MemberNotFoundException

--- src/main/java/com/tarakki/common/exceptionHandling/MemberNotFoundException.java
+++ src/main/java/com/tarakki/common/exceptionHandling/MemberNotFoundException.java
@@ -6,4 +6,8 @@ public class MemberNotFoundException extends RuntimeException {
     public MemberNotFoundException(UUID userId) {
         super("User not found at id:" +userId);
     }
+
+    public MemberNotFoundException(String email) {
+        super("Member not found with email: " + email);
+    }
 }
