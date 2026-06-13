35df38f99094662a6ade42be0f8001c094b22eaa
Manmeet Kaur <meetka2005@gmail.com>
2026-06-13 19:39:15 +0530

    Refactor: Rename Organisation to Organization in exceptions and fix member ID column

--- src/main/java/com/tarakki/common/exceptionHandling/MemberNotFoundException.java
+++ src/main/java/com/tarakki/common/exceptionHandling/MemberNotFoundException.java
@@ -1,8 +1,5 @@
 package com.tarakki.common.exceptionHandling;
 
-import org.springframework.http.HttpStatus;
-import org.springframework.web.bind.annotation.ResponseStatus;
-
 import java.util.UUID;
 
     @ResponseStatus(HttpStatus.NOT_FOUND)
