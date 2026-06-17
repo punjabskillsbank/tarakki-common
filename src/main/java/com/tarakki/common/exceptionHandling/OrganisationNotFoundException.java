09f5b0bb5692c463869931ea901de373d8585117
Manmeet Kaur <meetka2005@gmail.com>
2026-06-18 01:07:50 +0530

    TK_48: Fixed Organization spelling in exception

--- src/main/java/com/tarakki/common/exceptionHandling/OrganisationNotFoundException.java
+++
@@ -1,8 +0,0 @@
-package com.tarakki.common.exceptionHandling;
-
-public class OrganisationNotFoundException extends RuntimeException {
-
-    public OrganisationNotFoundException(Long orgId) {
-        super("Organisation with id " + orgId + " not found");
-    }
-}
